32c9909f779117f41e48d22e741af0b0a9b12b59
Gunnar Farnebäck <gunnar@lysator.liu.se>
2001-12-13 22:21:54 +0000

    Trivial bugfix.

--- engine/combination.c
+++ engine/combination.c
@@ -637,7 +637,7 @@ do_atari_atari(int color, int *attack_point, int *defense_point,
     if (!trymove(apos, color, "do_atari_atari-A", str, EMPTY, NO_MOVE))
       continue;
 	
-    if (!IS_STONE(str)) {
+    if (!IS_STONE(board[str])) {
       /* Error situation. This could be caused by a wrong matcher status. */
       if (save_verbose || (debug & DEBUG_ATARI_ATARI))
 	gprintf("%oError condition found by atari_atari\n", str, apos);
