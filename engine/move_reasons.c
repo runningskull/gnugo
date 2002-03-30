c51abb5055af930fbc9866c4247a64f690349f4d
Daniel Bump <bump@sporadic.stanford.edu>
2002-03-30 23:44:44 +0000

    took down asserts in add_replacement_move

--- engine/move_reasons.c
+++ engine/move_reasons.c
@@ -1396,8 +1396,10 @@ add_replacement_move(int from, int to)
 
   ASSERT_ON_BOARD1(from);
   ASSERT_ON_BOARD1(to);
+#if 0
   ASSERT1(board[from] == EMPTY, from);
   ASSERT1(board[to] == EMPTY, to);
+#endif
 
   cc = replacement_map[to];
 
