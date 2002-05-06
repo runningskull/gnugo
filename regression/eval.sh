27017ffd5b89e4d4e1ef7b068e1368b59e3dfbbc
Daniel Bump <bump@sporadic.stanford.edu>
2002-05-06 14:06:01 +0000

    added arend_3_2.1a to CVS

--- regression/eval.sh
+++ regression/eval.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-GNUGO=../interface/gnugo
+if test ! "$GNUGO"; then
+	GNUGO=../interface/gnugo
+fi
 
 tstfile=$1
 options=$2   # May be omitted
-options2=$3  # May be omitted  FIXME: Do multiple options better.
+options2=$3  # May be omitted
+options3=$4  # May be omitted
+options4=$5  # May be omitted  FIXME: Do multiple options better.
 
-$GNUGO --quiet $options $options2 --mode gtp <$tstfile |\
+$GNUGO --quiet $options $options2 $options3 $options4 --mode gtp <$tstfile |\
 	awk -f regress.awk tst=$tstfile verbose=1
--- regression/regress.sh
+++ regression/regress.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-GNUGO=../interface/gnugo
+if test ! "$GNUGO" ; then
+	GNUGO=../interface/gnugo
+fi
 
 srcdir=$1
 tstfile=$2
 options=$3   # May be omitted
-options2=$4  # May be omitted  FIXME POST3.0: Do multiple options nicer.
+options2=$4  # May be omitted
+options3=$5  # May be omitted
+options4=$6  # May be omitted  FIXME POST3.0: Do multiple options nicer.
 
-$GNUGO --quiet $options $options2 --mode gtp <$tstfile |\
+$GNUGO --quiet $options $options2 $options3 $options4 --mode gtp <$tstfile |\
 	awk -f $srcdir/regress.awk tst=$tstfile
