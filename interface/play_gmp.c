652f247752073f2dbc21feec3a6ce242fe3b1f8b
Daniel Bump <bump@sporadic.stanford.edu>
2002-11-16 05:04:31 +0000

    added evan_3_12.11

--- interface/play_gmp.c
+++ interface/play_gmp.c
@@ -82,8 +82,10 @@ play_gmp(Gameinfo *gameinfo)
   gameinfo->handicap = gmp_handicap(ge);
   gnugo_clear_board(gmp_size(ge));
 
+#if ORACLE
   if (metamachine && oracle_exists)
     oracle_clear_board(gnugo_get_boardsize());
+#endif
 
   /* 
    * The specification of the go modem protocol doesn't even discuss
@@ -224,8 +226,11 @@ play_gmp(Gameinfo *gameinfo)
     if (message == gmp_err)
       break;
   }
+
+#if ORACLE
   if (metamachine && oracle_exists)
     dismiss_oracle();
+#endif
 
   if (!quiet)
     fprintf(stderr, "gnugo going down\n");
