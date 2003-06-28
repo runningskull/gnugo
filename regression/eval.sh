ec7f11c796fb722df21e102fba7f1d05d9d919ee
Daniel Bump <bump@sporadic.stanford.edu>
2003-06-28 13:07:02 +0000

    teun_3_22.1

--- regression/eval.sh
+++ regression/eval.sh
@@ -4,11 +4,15 @@ if test ! "$GNUGO"; then
 	GNUGO=../interface/gnugo
 fi
 
+if test ! "$RD"; then
+	RD=.
+fi
+
 tstfile=$1
-options=$2   # May be omitted
+options1=$2   # May be omitted
 options2=$3  # May be omitted
 options3=$4  # May be omitted
 options4=$5  # May be omitted  FIXME: Do multiple options better.
 
-$GNUGO --quiet $options $options2 $options3 $options4 --mode gtp <$tstfile |\
-	awk -f regress.awk tst=$tstfile verbose=1
+$GNUGO --quiet $options1 $options2 $options3 $options4 --mode gtp <$tstfile |\
+	awk -f $RD/regress.awk tst=$tstfile verbose=1
