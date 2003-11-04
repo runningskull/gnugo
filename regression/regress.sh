a87e6d52722e77ed677acfb7d173012a90838876
Gunnar Farnebäck <gunnar@lysator.liu.se>
2003-11-04 13:32:27 +0000

    thi_5_2.1

--- regression/regress.sh
+++ regression/regress.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
+# regress.sh SRCDIR TSTFILE [OPTIONS ...]
+#
+# Use ../interface/gnugo to run test TSTFILE in SRCDIR (first changing
+# directory to SRCDIR).  OPTIONS are passed directly to gnugo.
 
 if test ! "$GNUGO" ; then
-	GNUGO=../interface/gnugo
+	GNUGO=`cd ../interface && pwd`/gnugo
+	if test ! -x "$GNUGO" ; then
+		echo ERROR: no such program: $GNUGO
+		exit 1
+	fi
 fi
 
 srcdir=$1
 tstfile=$2
-options=$3   # May be omitted
-options2=$4  # May be omitted
-options3=$5  # May be omitted
-options4=$6  # May be omitted  FIXME: Do multiple options nicer.
+shift
+shift
 
-$GNUGO --quiet $options $options2 $options3 $options4 --mode gtp <$tstfile |\
-	awk -f $srcdir/regress.awk tst=$tstfile
+cd $srcdir
+$GNUGO --quiet "$@" --mode gtp < $tstfile \
+	| awk -f regress.awk tst=$tstfile
+
+# regress.sh ends here
